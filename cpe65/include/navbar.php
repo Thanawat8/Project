fbe4c3aadb88496a651659f6964715c04d3e20a1
Thanawat8 <109579441+Thanawat8@users.noreply.github.com>
2025-10-24 03:45:44 +0700

    Add files via upload

---
+++ cpe65/include/navbar.php
@@ -0,0 +1,33 @@
+<?php
+
+?>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="sales_summary.php">JT Entertainment</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <li class="nav-item">
+                    <a class="nav-link" href="DB_product_search.php">
+                        <i class="bi-search"></i>&nbsp;ค้นหาสินค้า
+                    </a>
+                </li>
+                
+                <li class="nav-item">
+                    <a class="nav-link" href="sales_order.php">
+                        <i class="bi bi-cart-plus"></i>&nbsp;การขายสินค้า
+                    </a>
+                </li>
+                 
+                <a class="nav-link" href="sales_summary.php">
+                        <i class="bi bi-clipboard-data"></i>&nbsp;ตรวจสอบยอดขายสินค้า
+                    </a>
+                    
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
